c070c4b35fd45296339a25174e038c218c02e5b0
ayaan <ayaanshafi14038@gmail.com>
2026-07-18 15:02:30 +0530

    Revert "Redis disabled"
    This reverts commit d103713fbf016485cf151377a0096a0fc6e4d659.

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const cors = require("cors");
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 const borrowRoutes = require('./routes/borrowRoutes');
-// const redisClient = require('./config/redis')
+const redisClient = require('./config/redis')
 const rateLimit = require('express-rate-limit');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const limiter = rateLimit({
     windowMs:15*60*1000,
-    max:100,
+    max:5,
     message:{message: "Too many requests, please try again later"}
 })
 
@@ -20,7 +19,6 @@ const limiter = rateLimit({
 connectDB();
 app.use(limiter);
 app.use(express.json());
-app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
