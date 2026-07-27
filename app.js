84139cee29ad3a523bf8e782c3d49dc7d8233535
Ayaan Shafi <ayaanshafi14038@gmail.com>
2026-07-27 18:04:39 +0530

    Serve frontend as ststic files from Express

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 const borrowRoutes = require('./routes/borrowRoutes');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
 const app = express();
+app.use(express.static('public'));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -14,6 +16,7 @@ const limiter = rateLimit({
 
 app.use(limiter);
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
