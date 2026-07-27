65ebbc3799676ec4c88c12e322f0c5788f182e71
Ayaan Shafi <ayaanshafi14038@gmail.com>
2026-07-27 18:20:02 +0530

    Use REDIS_URL environment variable for Redis connection

--- config/redis.js
+++ config/redis.js
@@ -1,16 +1,17 @@
 const redis = require('redis');
+
 const client = redis.createClient({
-    host: 'localhost',
-    port: 6379
+  url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 
-client.on('error', (err)=>{
-    console.log('Redis error', err);
-})
+client.on('error', (err) => {
+  console.log('Redis error', err);
+});
 
-client.on('connect', ()=>{
-    console.log("Redis connected successfully");
-})
-client.connect()
+client.on('connect', () => {
+  console.log("Redis connected successfully");
+});
 
-module.exports = client
+client.connect();
+
+module.exports = client;
