539c3c7be7b1cb53c801529d75b1155372cbdae9
Ayaan Shafi <ayaanshafi14038@gmail.com>
2026-07-28 12:45:55 +0530

    email and password validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,10 +2,29 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
+function isStrongPassword(password){
+    const hasMinLength = password.length>=8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecial = /[^A-Za-z0-9]/.test(password);
+
+    return hasMinLength && hasUppercase && hasNumber && hasSpecial;
+}
+
 const signup = async(req, res, next) => {
     try {
         const {name, email, username, password, DOB} = req.body
         
+        if (!isValidEmail(email)){
+            return res.status(400).json({message: "Enter a valid email"})
+        }
+        if(!isStrongPassword(password)){
+            return res.status(400).json({message: "Invalid password: Minimum 8 characters, must contain uppercase, special character and number"})
+        }
         const existingUser = await User.findOne({$or: [{email}, {username}]})
         if(existingUser){
             return res.status(400).json({message: "User already exists"})
